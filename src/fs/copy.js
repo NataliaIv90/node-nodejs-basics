cb0410f974a9cafc49b5dbcb7c4b4ee86076e58a
Natalia Ivantsova <ivantsovana@gmail.com>
2024-01-29 11:39:23 +0200

    feat: add copy func to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,28 @@
+import { cp, existsSync, mkdirSync } from 'node:fs'
+
 const copy = async () => {
-    // Write your code here 
+    const folderName = './files';
+    const newFolderName = './files_copy'
+
+    try {
+        if (!existsSync(folderName) || existsSync(newFolderName)) {
+            throw new Error('FS operation failed');
+        }
+
+        mkdirSync('files_copy');
+        cp(folderName, newFolderName, { recursive: true }, (err) => {
+            if (err) {
+                throw new Error('Error occured. Can\'t proceed');
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await copy();
+
+/**
+ * copy.js - implement function that copies folder files files with all its content into folder files_copy at the same level 
+ * (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)
+ */
