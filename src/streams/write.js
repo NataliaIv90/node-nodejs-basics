759e772d8777743a9f4361728e5fc0de965ce3d9
Natalia Ivantsova <ivantsovana@gmail.com>
2024-01-29 15:15:59 +0200

    feat: add write.js in streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from 'node:fs';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream('files/fileToWrite.txt', { encoding: 'utf8' });
+
+    process.stdin.on('data', (chunk) => {
+        writeStream.write(chunk);
+    });
 };
 
 await write();
