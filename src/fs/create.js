1dd4c8fb65673493ecebdd209bc7c96a8b200746
Natalia Ivantsova <ivantsovana@gmail.com>
2024-01-29 10:33:30 +0200

    feat: add create func to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { writeFile } from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const content = 'I am fresh and young';
+        await writeFile('files/fresh.txt', content);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await create();
+
+/*
+create.js - 
+implement function that creates new file fresh.txt with content 
+'I am fresh and young' inside of the files folder
+(if file already exists Error with message FS operation failed must be thrown)
+*/
