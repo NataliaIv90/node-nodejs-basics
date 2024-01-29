cd64e12eeb44671e94a6c0c94c384194ddc374a2
Natalia Ivantsova <ivantsovana@gmail.com>
2024-01-29 15:30:05 +0200

    feat: add compress and decompress func

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,12 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import zlib from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('files/archive.gz');
+    const writeStream = createWriteStream('files/fileToCompress.txt', { encoding: 'utf8' });
+    const gunzip = zlib.createGunzip();
+
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
